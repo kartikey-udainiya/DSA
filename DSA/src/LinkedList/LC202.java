2093e982e5edcf59cd8f14677c4e4879ef071c99
kartikey-udainiya-centralogic <kartikey.udainiya@centralogic.net>
2026-07-09 10:58:11 +0530

    LC202 solved

---
+++ DSA/src/LinkedList/LC202.java
@@ -0,0 +1,30 @@
+package LinkedList;
+
+import java.sql.SQLOutput;
+
+public class LC202 {
+
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do{
+            slow=findSquare(slow);
+            fast=findSquare(findSquare(fast));
+        }while(slow!=fast);
+        if(slow==1) return true;
+
+        return false;
+    }
+
+    private int findSquare(int num){
+
+        int ans = 0;
+        while(num>0){
+            int rem = num%10;
+            ans+=rem*rem;
+            num/=10;
+        }
+        return ans;
+    }
+}
---
+++ DSA/src/LinkedList/Main.java
@@ -0,0 +1,10 @@
+package LinkedList;
+
+import LinkedList.LC202;
+
+class Main {
+    public static void main(String[] args) {
+        LC202 a =  new LC202();
+        a.isHappy(12);
+    }
+}
