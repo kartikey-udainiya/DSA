28b4a39dcfbfbdfb351b185175eb5245ce3c9a51
kartikey-udainiya <kartikeyudainiya@gmail.com>
2026-08-30 07:28:33 +0530

    SegmentTree

--- DSA/src/BinaryTree/Index.md
+++ DSA/src/BinaryTree/Index.md
@@ -1,3 +1,4 @@
 1. read the code in : BinaryTree.java
 2. Then BST.java with BST.md
 3. AVL.java
+4. SegmentTREE.java
---
+++ DSA/src/BinaryTree/SegmentTree.java
@@ -0,0 +1,84 @@
+public class SegmentTree {
+
+    class Node{
+        private int value;
+        private int startInterval;
+        private int endInterval;
+        private Node left;
+        private Node right;
+
+        public Node(int startInterval, int endInterval){
+            this.startInterval=startInterval;
+            this.endInterval= endInterval;
+        }
+    }
+    private Node root;
+
+    public SegmentTree(){
+
+    }
+
+    public void populate(int[] arr){
+        root = construct(arr , 0 , arr.length-1);
+    }
+
+    private Node construct(int arr [], int start, int end){
+        if(start==end){
+            Node leaf = new Node(start,end);
+            leaf.value=arr[start];
+            return leaf;
+        }
+
+        Node node = new Node(start,end);
+
+        int mid = (start+end)/2;
+
+        node.left = construct(arr,start,mid);
+        node.right = construct(arr,mid+1,end);
+
+        node.value = node.left.value + node.right.value;
+
+        return node;
+
+    }
+
+    public int query(int start , int end){
+        return query(root,start,end);
+    }
+
+    private int query(Node node,int start, int end){
+        // completely inside
+        if(start <= node.startInterval && node.endInterval <= end) {
+            return node.value;
+        }
+        // completely outside
+        if(node.startInterval > end || node.endInterval < start) {
+            return 0;
+        }
+        // partial overlap
+        return query(node.left, start, end)
+                + query(node.right, start, end);
+    }
+
+    public void update(int value , int index){
+        update(root,value,index);
+    }
+
+    private int update(Node node, int value , int index){
+        if(node.startInterval<=index && node.endInterval>=index){
+            if(node.startInterval==index && node.endInterval==index){
+                node.value=value;
+                return node.value;
+            }else{
+                int leftans = update(node.left,value,index);
+                int rightans = update(node.right,value,index);
+
+                node.value = leftans + rightans;
+                return node.value;
+            }
+        }
+
+        return node.value;
+
+    }
+}
